61e9aa1be3daa0270f1777e230369fcc31043d06
krishnasankar <sankarkrishna689@gmail.com>
2024-03-23 17:03:43 +0530

    build changes

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,7 @@ python3 -m venv .venv
 source .venv/bin/activate  
 ./.venv/bin/python3 -m pip install --upgrade pip
 ./.venv/bin/python3 -m pip install -r requirements.txt
+./.venv/bin/python3 manage.py makemigrations
+./.venv/bin/python3 manage.py migrate
 ./.venv/bin/python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
